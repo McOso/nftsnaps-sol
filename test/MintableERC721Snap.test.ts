56d1614c33cc3187e15cf95a4373e782632d7bff
Ryan McPeck <mcoso.dev@gmail.com>
2023-03-13 22:35:24 -0400

    feat: tests started

---
+++ test/MintableERC721Snap.test.ts
@@ -0,0 +1,52 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { Contract, ContractFactory } from 'ethers';
+import { ethers } from 'hardhat';
+
+const { getSigners, utils } = ethers;
+const { parseEther: toWei } = utils;
+
+const contractInformation = {
+  name: 'Test Snap',
+  description: 'This is a test snap',
+  image: 'ipfs://QmXxZWr5AQf25yu1UswNm2cfGbaUbR5U3ejH1WfFEP8f1e',
+  externalLink: 'https://testing.snap',
+  sellerFeeBasisPoints: '100',
+  feeRecipient: '0x0000000000000000000000000000000000000000',
+};
+
+const MINT_FEE = toWei('0.0000092');
+
+describe('MintableERC721Snap', () => {
+  let wallet0: SignerWithAddress;
+  let wallet1: SignerWithAddress;
+  let wallet2: SignerWithAddress;
+  let MintableERC721Snap: Contract;
+  let MintableERC721Factory: ContractFactory;
+
+  before(async () => {
+    [wallet0, wallet1, wallet2] = await getSigners();
+    MintableERC721Factory = await ethers.getContractFactory('MintableERC721Snap');
+  });
+
+  beforeEach(async () => {
+    MintableERC721Snap = await MintableERC721Factory.deploy('Test Snap', 'NFTSNAP', contractInformation,'ipfs://QmXxZWr5AQf25yu1UswNm2cfGbaUbR5U3ejH1WfFEP8f1e', 'ipfs://QmRZ86jmHScFm94hoED2FmB6SjqpuACgy6VYN5nTibxwSB', MINT_FEE, wallet0.address, wallet1.address, 0);
+  });
+
+  describe('mint(address to)', () => {
+    it('should SUCCEED to mint NFT #1', async () => {
+      await MintableERC721Snap.mint(wallet0.address, {value: MINT_FEE});
+      expect(await MintableERC721Snap.ownerOf(1)).to.be.equal(wallet0.address);
+    });
+    it('should FAIL to mint NFT #1 - no funds', async () => {
+      await expect(
+        MintableERC721Snap.mint(wallet0.address)
+      ).to.be.revertedWith("NFTSnap:insufficient-amount");
+    });
+    it('should FAIL to mint NFT #1 - not enough funds', async () => {
+      await expect(
+        MintableERC721Snap.mint(wallet0.address, {value: toWei('0.0000091')})
+      ).to.be.revertedWith("NFTSnap:insufficient-amount");
+    });
+  });
+});
---
+++ test/SnapFactory.test.ts
@@ -0,0 +1,71 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { deployMockContract, MockContract } from 'ethereum-waffle';
+import { Contract, ContractFactory } from 'ethers';
+import { artifacts, ethers } from 'hardhat';
+import { Artifact } from 'hardhat/types';
+
+const { getSigners, utils } = ethers;
+const { parseEther: toWei } = utils;
+
+const contractInformation = {
+  name: 'Test Snap',
+  description: 'This is a test snap',
+  image: 'ipfs://QmXxZWr5AQf25yu1UswNm2cfGbaUbR5U3ejH1WfFEP8f1e',
+  externalLink: 'https://testing.snap',
+  sellerFeeBasisPoints: '100',
+  feeRecipient: '0x0000000000000000000000000000000000000000',
+};
+
+const MINT_FEE = toWei('0.0000092');
+
+describe('SnapFactory', () => {
+  let wallet0: SignerWithAddress;
+  let wallet1: SignerWithAddress;
+  let wallet2: SignerWithAddress;
+  let SnapFactoryContract: Contract;
+  let SnapFactory: ContractFactory;
+
+  let MintableSnapMock: MockContract;
+  let MintableSnapArtifact: Artifact;
+
+  before(async () => {
+    [wallet0, wallet1, wallet2] = await getSigners();
+    SnapFactory = await ethers.getContractFactory('SnapFactory');
+
+    MintableSnapArtifact = await artifacts.readArtifact('MintableERC721Snap');
+    MintableSnapMock = await deployMockContract(wallet0, MintableSnapArtifact.abi);
+    // await MintableERC20Mock.mock.getAmount.returns(21554685);
+    // await MintableERC20Mock.mock.getAverageBalanceBetween.returns(4654875000);
+    // await MintableERC20Mock.mock.getAverageBalanceBetween
+    //   .withArgs(wallet1.address, 1673802000, 1675184400)
+    //   .returns(0);
+    // await MintableERC20Mock.mock.getSomethingBool.returns(false);
+    // await MintableERC20Mock.mock.getSomethingBool
+    //   .withArgs(wallet1.address, 1673802000, 1675184400)
+    //   .returns(true);
+  });
+
+  beforeEach(async () => {
+    SnapFactoryContract = await SnapFactory.deploy();
+  });
+
+  describe('createSnap()', () => {
+    it('should SUCCEED to create a snap', async () => {
+      const result = await SnapFactoryContract.createSnap('Test Snap', 'NFTSNAP', contractInformation,'ipfs://QmXxZWr5AQf25yu1UswNm2cfGbaUbR5U3ejH1WfFEP8f1e', 'ipfs://QmRZ86jmHScFm94hoED2FmB6SjqpuACgy6VYN5nTibxwSB', MINT_FEE, wallet0.address, wallet1.address, 0);
+      expect(result.hash).to.not.be.undefined;
+      expect(result.confirmations).to.be.greaterThan(0);
+      await expect(result).to.emit(SnapFactoryContract, 'SnapMade');
+      expect( (await SnapFactoryContract.getActiveSnaps()).length ).to.equal(1);
+    });
+
+    it('should SUCCEED to create a snap with creator fee', async () => {
+      const result = await SnapFactoryContract.createSnap('Test Snap', 'NFTSNAP', contractInformation,'ipfs://QmXxZWr5AQf25yu1UswNm2cfGbaUbR5U3ejH1WfFEP8f1e', 'ipfs://QmRZ86jmHScFm94hoED2FmB6SjqpuACgy6VYN5nTibxwSB', MINT_FEE, wallet0.address, wallet1.address, toWei('0.045'));
+      expect(result.hash).to.not.be.undefined;
+      expect(result.confirmations).to.be.greaterThan(0);
+      await expect(result).to.emit(SnapFactoryContract, 'SnapMade');
+      expect( (await SnapFactoryContract.getActiveSnaps()).length ).to.equal(1);
+    });
+
+  });
+});
